b786d16a7bfcbde8ffa0e5360e27e350c29ffcdc
Carolyn.Maynard <carolyn.maynard@rtx.com>
2025-12-15 17:10:34 -0800

    Don't mix relative and absolute naming when importing same module.

--- src/topoflow_glacier/__init__.py
+++ src/topoflow_glacier/__init__.py
@@ -1,5 +1,5 @@
 from ._version import __version__
-from .bmi.bmi_topoflow_glacier import BmiTopoflowGlacier
-from .bmi.logger import configure_logging, MODULE_NAME
+from topoflow_glacier.bmi.bmi_topoflow_glacier import BmiTopoflowGlacier
+from topoflow_glacier.bmi.logger import configure_logging, MODULE_NAME
 
 __all__ = ["__version__", "BmiTopoflowGlacier", "configure_logging", "MODULE_NAME"]
