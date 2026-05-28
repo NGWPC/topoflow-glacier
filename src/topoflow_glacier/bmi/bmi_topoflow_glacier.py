1b11d193c354d7258eef489c55911c419b8e2f38
Carolyn.Maynard <carolyn.maynard@rtx.com>
2026-05-28 15:05:40 -0700

    Update stdout format to match ewts format. Add file meta data for all but INFO log level.

--- src/topoflow_glacier/bmi/bmi_topoflow_glacier.py
+++ src/topoflow_glacier/bmi/bmi_topoflow_glacier.py
@@ -14,6 +14,7 @@ from topoflow_glacier.bmi.config import TopoflowGlacierConfig
 from topoflow_glacier.physics import solar_funcs as solar
 from topoflow_glacier.physics.context import Context, build_context
 
+from datetime import datetime, timezone
 import logging
 LOG = logging.getLogger("TFGLACR") # IMPORTANT! Use exact string from ewts.modules.TOPOFLOW_GLACIER_ID
 try:
@@ -142,20 +143,42 @@ def load_static_attributes(cfg: dict[str, Any], state: Context):
         value = cfg[internal_name]
         state.set_value(external_name, bmi_array([value]))
 
+class StdoutStyleFormatter(logging.Formatter):
+
+    INFO_FORMAT = (
+        "%(asctime)s %(name)-8s %(levelname)-7s %(message)s"
+    )
+
+    DETAILED_FORMAT = (
+        "%(asctime)s %(name)-8s %(levelname)-7s "
+        "%(message)s "
+        "[%(filename)s.%(funcName)s(L%(lineno)s)]"
+    )
+
+    def format(self, record):
+        if record.levelno == logging.INFO:
+            self._style._fmt = self.INFO_FORMAT
+        else:
+            self._style._fmt = self.DETAILED_FORMAT
+
+        return super().format(record)
+    
+    def formatTime(self, record, datefmt=None):
+        dt = datetime.fromtimestamp(record.created, tz=timezone.utc)
+        return dt.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
+
+
 def _configure_stdout_logging():
     LOG.setLevel(logging.INFO)
 
     if not LOG.handlers:
         handler = logging.StreamHandler()
         handler.setLevel(logging.INFO)
-        handler.setFormatter(logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - "
-            "[%(filename)s:%(lineno)s - %(funcName)s]: %(message)s"
-        ))
+        handler.setFormatter(StdoutStyleFormatter())
         LOG.addHandler(handler)
 
     LOG.propagate = False
-    
+
 class BmiTopoflowGlacier(BmiBase):
     """BMI composition wrapper for TopoflowGlacier"""
 
@@ -169,7 +192,7 @@ class BmiTopoflowGlacier(BmiBase):
                 configure_existing_logger(LOG)
             else:
                 _configure_stdout_logging()
-                LOG.warning("EWTS importable but EWTS_USE_NGEN_BRIDGE not on. Falling back to default logging.")
+                LOG.warning("ewts package installed but EWTS_USE_NGEN_BRIDGE not on. Falling back to default logging.")
         else:
             _configure_stdout_logging()
 
